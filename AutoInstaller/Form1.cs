7570a88841b932617cd32823870254688d4ac150
FRUDNIC3 <FRUDNIC3@HUDDQ882H2>
2018-08-06 14:54:59 +0200

    Initial Components Error javítva.

--- AutoInstaller/Form1.cs
+++ AutoInstaller/Form1.cs
@@ -16,23 +16,7 @@ namespace AutoInstaller
         {
             InitializeComponent();
         }
-//      Severity Code    Description Project File Line    Suppression State
-//Error CS0111  Type 'Form1' already defines a member called 'InitializeComponent' with the same parameter types AutoInstaller   C:\Users\azsizsi1\Desktop\közösapp\AutoInstaller\AutoInstaller\Form1.Designer.cs	29	N/A
-
-
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // Form1
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Name = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
-            /*BeleírtamTeszt*/
-        }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
 
