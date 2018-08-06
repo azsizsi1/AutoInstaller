b61a57a7d8da5f6da69e4ea4882696b9b011af68
azsizsi1 <42037239+azsizsi1@users.noreply.github.com>
2018-08-06 14:41:34 +0200

    in progress

--- AutoInstaller/Form1.Designer.cs
+++ AutoInstaller/Form1.Designer.cs
@@ -28,11 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ItemChooser = new System.Windows.Forms.CheckedListBox();
+            this.ItemList = new System.Windows.Forms.ListBox();
+            this.starter = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ItemChooser
+            // 
+            this.ItemChooser.FormattingEnabled = true;
+            this.ItemChooser.Items.AddRange(new object[] {
+            "Outlook",
+            "Directx",
+            "Appshare"});
+            this.ItemChooser.Location = new System.Drawing.Point(13, 13);
+            this.ItemChooser.Name = "ItemChooser";
+            this.ItemChooser.ScrollAlwaysVisible = true;
+            this.ItemChooser.Size = new System.Drawing.Size(570, 349);
+            this.ItemChooser.TabIndex = 0;
+            // 
+            // ItemList
+            // 
+            this.ItemList.FormattingEnabled = true;
+            this.ItemList.Location = new System.Drawing.Point(589, 13);
+            this.ItemList.Name = "ItemList";
+            this.ItemList.Size = new System.Drawing.Size(153, 316);
+            this.ItemList.TabIndex = 1;
+            // 
+            // starter
+            // 
+            this.starter.Location = new System.Drawing.Point(589, 336);
+            this.starter.Name = "starter";
+            this.starter.Size = new System.Drawing.Size(153, 26);
+            this.starter.TabIndex = 2;
+            this.starter.Text = "Install Item(s)";
+            this.starter.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(745, 373);
+            this.Controls.Add(this.starter);
+            this.Controls.Add(this.ItemList);
+            this.Controls.Add(this.ItemChooser);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -41,6 +77,9 @@
 
         #endregion
 
+        private System.Windows.Forms.CheckedListBox ItemChooser;
+        private System.Windows.Forms.ListBox ItemList;
+        private System.Windows.Forms.Button starter;
     }
 }
 
