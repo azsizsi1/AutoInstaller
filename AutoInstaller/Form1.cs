3aeead4df3bb967c34b8b7ed5fffc86eb1e75ca8
azsizsi1 <42037239+azsizsi1@users.noreply.github.com>
2018-08-07 15:34:17 +0200

    Elkezdtem a feltöltést

--- AutoInstaller/Form1.cs
+++ AutoInstaller/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +14,10 @@ namespace AutoInstaller
 {
     public partial class Form1 : Form
     {
+
+        public int CountOfPrograms = 0;
+        string location;
+        string txtline = "";
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +25,40 @@ namespace AutoInstaller
         
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                StreamReader route = File.OpenText("C:/desktop/installroutes.txt");
+                while ((txtline = route.ReadLine()) != null)
+                {
+                    string[] database = txtline.Split(';');
+                    ItemChooser.Items.Add(new Tolt(database[0], database[1], database[2]));
+                    CountOfPrograms++;
+                }
+                route.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             /*Ide kéne megírni a beolvasást, hogy már a form betöltésénél legyen 
              adat a ItemChooser-ben. Try-catch-ben mindenképp.*/
         }
+    }
+
+
+
+    public class Tolt
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Path { get; set; }
+
+        public Tolt(string name, string version, string path)
+        {
+            Name = name;
+            Version = version;
+            Path = path;
+        }
+
     }
 }
